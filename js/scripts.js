74f013a44a32c188bf276bf2cfc972e00fe7e199
Curtis Gray <87861225+curtisgry@users.noreply.github.com>
2022-02-19 07:45:52 -0500

    add pokemon data

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+const pokemonList = [
+        {
+                name: 'Bulbasaur',
+                height: 0.7,
+                types: ['grass', 'poison'],
+        },
+        {
+                name: 'Charmander',
+                height: 0.6,
+                types: ['fire'],
+        },
+        {
+                name: 'Squirtle',
+                height: 0.5,
+                types: ['water'],
+        },
+];
