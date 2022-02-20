6671dd598f589b7dc4437081084e019f1b0fbca2
Curtis Gray <87861225+curtisgry@users.noreply.github.com>
2022-02-20 07:40:40 -0500

    add IIFE for pokemon data

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,38 @@
-const pokemonList = [
-        {
-                name: 'Bulbasaur',
-                height: 0.7,
-                types: ['grass', 'poison'],
-        },
-        {
-                name: 'Charmander',
-                height: 0.6,
-                types: ['fire'],
-        },
-        {
-                name: 'Squirtle',
-                height: 0.5,
-                types: ['water'],
-        },
-];
+const pokemonRepository = (function () {
+        const pokemonList = [
+                {
+                        name: 'Bulbasaur',
+                        height: 0.7,
+                        types: ['grass', 'poison'],
+                },
+                {
+                        name: 'Charmander',
+                        height: 0.6,
+                        types: ['fire'],
+                },
+                {
+                        name: 'Squirtle',
+                        height: 0.5,
+                        types: ['water'],
+                },
+        ];
 
-// Display info from all pokemon in list
-pokemonList.forEach(function (item) {
+        function getAll() {
+                return pokemonList;
+        }
+
+        function add(item) {
+                pokemonList.push(item);
+        }
+
+        return {
+                getAll,
+                add,
+        };
+})();
+
+// Get list from within IIFE and display info from all pokemon in list
+pokemonRepository.getAll().forEach(function (item) {
         // Check for height over 0.6 to display additional text
         const specialText = item.height > 0.6 ? 'Wow thats big!' : '';
         // Write to page
